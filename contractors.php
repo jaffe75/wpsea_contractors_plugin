65fc6cb5382ebe47a3c9f0bed2bf1d54816950e5
Jaffe <jaffe75@gmail.com>
2012-08-28 00:04:35 -0700

    added plugin info into main php file

---
+++ contractors.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: WPSEA Contractors
+Plugin URI: https://github.com/jaffe75/wpsea_contractors_plugin.git
+Description: A plugin designed to add and display local area Wordpress contractors
+Version: 0.1.0
+Author: Jaffe Worley, Ben Lobaugh
+*/
+?>
